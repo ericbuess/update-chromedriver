212f6f31aff962d3ac316ad1f1a6d7da5e214566
Eric Buess <ericbuess@Erics-Mac-mini.local>
2024-06-12 17:42:12 -0500

    Make script compatible with Linux and Windows
    - Updated  to detect OS and use the appropriate Chrome binary path.
    - Updated  to include Chrome binary paths for macOS, Linux, and Windows.
    - Verified compatibility across different OS environments.

--- chrome_version.py
+++ chrome_version.py
@@ -1,12 +1,24 @@
 import subprocess
 import re
+import platform
+from config import CHROME_BINARY_PATH
 
 
 def get_chrome_version():
     """ Get the current version of Chrome installed """
     try:
+        system = platform.system()
+        if system == 'Darwin':  # macOS
+            command = [CHROME_BINARY_PATH['mac'], '--version']
+        elif system == 'Linux':  # Linux
+            command = [CHROME_BINARY_PATH['linux'], '--version']
+        elif system == 'Windows':  # Windows
+            command = [CHROME_BINARY_PATH['windows'], '--version']
+        else:
+            raise Exception(f"Unsupported operating system: {system}")
+
         process = subprocess.Popen(
-            ['/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', '--version'],
+            command,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
--- config.py
+++ config.py
@@ -1,2 +1,13 @@
+import platform
+
+system = platform.system()
+
 # Specify the path where you want to store the ChromeDriver
-CHROME_WEB_DRIVER_PATH = "/usr/local/bin/chromedriver"
+CHROME_WEB_DRIVER_PATH = "/usr/local/bin/chromedriver" if system != 'Windows' else "C:\\path\\to\\chromedriver.exe"
+
+# Specify the path to the Chrome binary for each operating system
+CHROME_BINARY_PATH = {
+    'mac': '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    'linux': '/usr/bin/google-chrome',
+    'windows': 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+}
